dca6467b9424e52c5a3c580db16acf3c7fa0ef09
iida-hayato <hayato.iida.0213@gmail.com>
2019-01-03 23:30:50 +0900

    remove ship scene

--- alife Shared/GameController.swift
+++ alife Shared/GameController.swift
@@ -9,65 +9,33 @@
 import SceneKit
 
 #if os(watchOS)
-    import WatchKit
+import WatchKit
 #endif
 
 #if os(macOS)
-    typealias SCNColor = NSColor
+typealias SCNColor = NSColor
 #else
-    typealias SCNColor = UIColor
+typealias SCNColor = UIColor
 #endif
 
 class GameController: NSObject, SCNSceneRendererDelegate {
 
-    let scene: SCNScene
-    let sceneRenderer: SCNSceneRenderer
-    
-    init(sceneRenderer renderer: SCNSceneRenderer) {
-        sceneRenderer = renderer
-        scene = SCNScene(named: "Art.scnassets/ship.scn")!
-        
-        super.init()
-        
-        sceneRenderer.delegate = self
-        
-        if let ship = scene.rootNode.childNode(withName: "ship", recursively: true) {
-            ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        }
-        
-        sceneRenderer.scene = scene
-    }
-    
-    func highlightNodes(atPoint point: CGPoint) {
-        let hitResults = self.sceneRenderer.hitTest(point, options: [:])
-        for result in hitResults {
-            // get its material
-            guard let material = result.node.geometry?.firstMaterial else {
-                return
-            }
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = SCNColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = SCNColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  let scene: SCNScene
+  let sceneRenderer: SCNSceneRenderer
+
+  init(sceneRenderer renderer: SCNSceneRenderer) {
+    sceneRenderer = renderer
+    scene = SCNScene(named: "Art.scnassets/main.scn")!
+
+    super.init()
+
+    sceneRenderer.delegate = self
+
+    sceneRenderer.scene = scene
+  }
+
+  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    // Called before each frame is rendered
+  }
 
 }
