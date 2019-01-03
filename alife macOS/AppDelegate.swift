b413b62544dd1c3d05b6dd2280ea9647b1cb669c
iida-hayato <hayato.iida.0213@gmail.com>
2019-01-03 23:20:15 +0900

    init

---
+++ alife macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  alife macOS
+//
+//  Created by hayato.iida on 2019/01/03.
+//  Copyright © 2019年 hayato.iida. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
